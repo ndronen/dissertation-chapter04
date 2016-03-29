74f83eeb93015bf25346ad537072f4f91e30f056
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-03-29 08:09:42 -0600

    Snapshot of models/real_errors/model.*

--- models/real_errors/model.py
+++ models/real_errors/model.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.setrecursionlimit(5000)
 
-import numpy as np
+from modeling.callbacks import DenseWeightNormCallback
 
 from keras.utils import np_utils
 from keras.models import Graph, Sequential
@@ -13,9 +13,23 @@ from modeling.builders import (build_embedding_layer,
     build_convolutional_layer, build_pooling_layer,
     build_dense_layer, build_optimizer, load_weights,
     build_hierarchical_softmax_layer)
+import modeling.data
 from modeling.dataset import HDF5FileDataset
 from modeling.callbacks import (PredictionCallback,
         ClassificationReport, ConfusionMatrix)
+
+class GraphMarshaller(modeling.data.GraphMarshaller):
+    def __init__(self, config):
+        self.config = config
+
+    def marshal(self, data, target=None):
+        return {
+                self.config.data_name[0]: data,
+                self.config.target_name: target
+                }
+
+    def unmarshal(self, output):
+        return output[self.config.target_name]
 
 class Identity(Layer):
     def get_output(self, train):
@@ -45,9 +59,9 @@ def build_graph(config, train_data):
     prev_layer = 'flatten'
 
     # Add some number of fully-connected layers without skip connections.
-    for i in range(config.n_fully_connected):
+    for i,n_hidden in enumerate(config.fully_connected):
         layer_name = 'dense%02d' %i
-        l = build_dense_layer(config, n_hidden=config.n_hidden)
+        l = build_dense_layer(config, n_hidden=n_hidden)
         graph.add_node(l, name=layer_name, input=prev_layer)
         prev_layer = layer_name
         if config.batch_normalization:
@@ -73,7 +87,7 @@ def build_graph(config, train_data):
         for layer_num in range(n_layers_per_residual_block):
             layer_name = 'h%s%02d' % (block_name, layer_num)
     
-            l = build_dense_layer(config, n_hidden=config.n_hidden)
+            l = build_dense_layer(config, n_hidden=config.fully_connected[-1])
             graph.add_node(l, name=layer_name, input=prev_layer)
             prev_layer = layer_name
     
@@ -93,10 +107,17 @@ def build_graph(config, train_data):
         graph.add_node(Activation('relu'), name=block_name+'relu', input=block_name+'output')
         prev_layer = block_input_layer = block_name+'relu'
 
-    graph.add_node(build_dense_layer(config, 2,
-        activation='softmax'), name='softmax', input=prev_layer)
+    graph.add_node(Dense(2), 
+            name='softmax_dense', input=prev_layer)
+    prev_layer = 'softmax_dense'
+    if config.batch_normalization:
+        graph.add_node(BatchNormalization(),
+                name='softmax_bn', input=prev_layer)
+        prev_layer = 'softmax_bn'
+    graph.add_node(Activation('softmax'),
+        name='softmax_activation', input=prev_layer)
 
-    graph.add_output(name=config.target_name, input='softmax')
+    graph.add_output(name=config.target_name, input='softmax_activation')
 
     load_weights(config, graph)
 
@@ -120,20 +141,15 @@ def build_sequential(config, train_data):
     model.add(build_pooling_layer(config, input_width=input_width))
     model.add(Flatten())
 
-    for i in range(config.n_fully_connected):
-        model.add(build_dense_layer(config, n_hidden=config.n_hidden))
+    for i,n_hidden in enumerate(config.fully_connected):
+        model.add(build_dense_layer(config, n_hidden=n_hidden))
         if config.batch_normalization:
             model.add(BatchNormalization())
         model.add(Activation('relu'))
 
-    for i in range(config.n_highway):
-        model.add(Highway(activation='relu'))
-        if config.batch_normalization:
-            model.add(BatchNormalization())
-
-    model.add(build_dense_layer(config, 2))
-    #if config.batch_normalization:
-    #    model.add(BatchNormalization())
+    model.add(build_dense_layer(config, n_hidden=2))
+    if config.batch_normalization:
+        model.add(BatchNormalization())
     model.add(Activation('softmax'))
 
     load_weights(config, model)
@@ -145,43 +161,50 @@ def build_sequential(config, train_data):
     return model
 
 
-def build_callbacks(model, config, X_validation=None, y_validation=None):
+def build_callbacks(model, config, X_validation=None, y_validation=None, marshaller=None):
     callbacks = []
 
     callbacks.append(keras.callbacks.EarlyStopping(
             patience=config.patience, verbose=1))
 
+    callbacks.append(DenseWeightNormCallback(config))
+
     if 'persistent' in config.mode:
         callbacks.append(keras.callbacks.ModelCheckpoint(
                 filepath=os.path.join(config.model_path, config.checkpoint_name),
                 save_best_only=config.save_best_only,
-                verbose=0))
+                verbose=1))
 
     pred_cb = PredictionCallback(
             X_validation,
-            logger=config.callback_logger,
+            logger=config.logger,
+            marshaller=marshaller,
             batch_size=config.batch_size)
 
     pred_callbacks = []
 
     if config.confusion_matrix:
-        if isinstance(model, keras.models.Sequential):
-            assert X_validation is not None
-            assert y_validation is not None
-            confusion_cb = ConfusionMatrix(
-                    X_validation, y_validation,
-                    logger=config.callback_logger)
-            pred_callbacks.append(confusion_cb)
+        if isinstance(model, keras.models.Graph):
+            assert marshaller is not None
+        assert X_validation is not None
+        assert y_validation is not None
+        confusion_cb = ConfusionMatrix(
+                X_validation, y_validation,
+                logger=config.logger,
+                marshaller=marshaller)
+        pred_callbacks.append(confusion_cb)
 
     if config.classification_report:
-        if isinstance(model, keras.models.Sequential):
-            assert X_validation is not None
-            assert y_validation is not None
-            report_cb = ClassificationReport(
-                    X_validation,
-                    y_validation,
-                    logger=config.callback_logger)
-            pred_callbacks.append(report_cb)
+        if isinstance(model, keras.models.Graph):
+            assert marshaller is not None
+        assert X_validation is not None
+        assert y_validation is not None
+        report_cb = ClassificationReport(
+                X_validation,
+                y_validation,
+                logger=config.logger,
+                marshaller=marshaller)
+        pred_callbacks.append(report_cb)
 
     for pcb in pred_callbacks:
         pred_cb.add(pcb)
@@ -193,9 +216,11 @@ def build_callbacks(model, config, X_validation=None, y_validation=None):
 
 
 def fit(config):
+    import numpy as np
+    np.random.seed(config.seed)
+
     if 'background' in config.mode:
-        sys.stdout, sys.stderr = setup_logging(
-                os.path.join(config.model_path, 'model.log'))
+        sys.stdout, sys.stderr = config.stdout, config.stderr
 
     train_data = HDF5FileDataset(
         config.train_path,
@@ -215,45 +240,63 @@ def fit(config):
         model = build_graph(config, train_data)
     else:
         model = build_sequential(config, train_data)
+        #model = build_sequential_simple(config, train_data)
+
+    config.logger('model has %d parameters' % model.count_params())
 
     class_weight = train_data.class_weights(
         config.class_weight_exponent,
         config.target_name)
 
+    rng = np.random.RandomState(seed=config.seed)
+
+    X_train = train_data.hdf5_file[config.data_name[0]].value
+    y_train = np_utils.to_categorical(
+            train_data.hdf5_file[config.target_name].value,
+            nb_classes=config.n_classes)
+    train_idx = rng.permutation(len(X_train))
+    X_train = X_train[train_idx]
+    y_train = y_train[train_idx]
+    #X_train = X_train[0:40000]
+    #y_train = y_train[0:40000]
+
+    X_validation = validation_data.hdf5_file[config.data_name[0]].value
+    y_validation = train_data.hdf5_file[config.target_name].value
+    y_validation_one_hot = np_utils.to_categorical(y_validation,
+            nb_classes=config.n_classes)
+    validation_idx = rng.permutation(len(X_validation))
+    X_validation = X_validation[validation_idx]
+    y_validation = y_validation[validation_idx]
+    y_validation_one_hot = y_validation_one_hot[validation_idx]
+    #X_validation = X_validation[0:40000]
+    #y_validation = y_validation[0:40000]
+    #y_validation_one_hot = y_validation_one_hot[0:40000]
+
     if config.n_residual_blocks > 0:
-        callbacks=build_callbacks(model, config)
+        marshaller = GraphMarshaller(config)
 
-        # Go through 1/10th of the training set every epoch.
-        samples_per_epoch = int(train_data.n/10)
+        callbacks=build_callbacks(model, config, 
+                X_validation, y_validation,
+                marshaller=marshaller)
 
-        model.fit_generator(train_data.generator(),
-                samples_per_epoch=samples_per_epoch,
-                nb_epoch=10*config.n_epochs,
-                #nb_worker=1,
+        model.fit(marshaller.marshal(X_train, y_train),
+                nb_epoch=config.n_epochs,
                 callbacks=callbacks,
-                validation_data=validation_data.generator(),
-                nb_val_samples=20000,
-                class_weight=class_weight)
+                validation_data=marshaller.marshal(X_validation, y_validation_one_hot),
+                class_weight=class_weight,
+                verbose=2 if 'background' in config.mode else 1)
+
+        #fraction = 100
+        #samples_per_epoch = int(train_data.n/fraction)
+        #model.fit_generator(train_data.generator(),
+        #        samples_per_epoch=samples_per_epoch,
+        #        nb_epoch=fraction*config.n_epochs,
+        #        #nb_worker=1,
+        #        callbacks=callbacks,
+        #        validation_data=validation_data.generator(),
+        #        nb_val_samples=40000,
+        #        class_weight=class_weight)
     else:
-        rng = np.random.RandomState(seed=config.seed)
-
-        X_train = train_data.hdf5_file[config.data_name[0]].value
-        y_train = np_utils.to_categorical(
-                train_data.hdf5_file[config.target_name].value,
-                nb_classes=config.n_classes)
-        train_idx = rng.permutation(len(X_train))
-        X_train = X_train[train_idx]
-        y_train = y_train[train_idx]
-
-        X_validation = validation_data.hdf5_file[config.data_name[0]].value
-        y_validation = train_data.hdf5_file[config.target_name].value
-        y_validation_one_hot = np_utils.to_categorical(y_validation,
-                nb_classes=config.n_classes)
-        validation_idx = rng.permutation(len(X_validation))
-        X_validation = X_validation[validation_idx]
-        y_validation = y_validation[validation_idx]
-        y_validation_one_hot = y_validation_one_hot[validation_idx]
-
         callbacks=build_callbacks(model, config, 
                 X_validation, y_validation)
 
@@ -264,4 +307,5 @@ def fit(config):
                 class_weight=class_weight,
                 show_accuracy=True,
                 batch_size=config.batch_size,
-                shuffle="batch")
+                shuffle=True,
+                verbose=2 if 'background' in config.mode else 1)
